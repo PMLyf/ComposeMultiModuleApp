69bdf11be827d463df5b371a00bab6a6d43f74f8
PMLyf <david.irvinge@gmail.com>
2024-06-03 10:09:50 -0400

    Created MainActivity and overrode the oncreate method

---
+++ app/src/main/java/com/pmlyf/composemultimoduleapp/Main.kt
@@ -0,0 +1,11 @@
+package com.pmlyf.composemultimoduleapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class Main: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
